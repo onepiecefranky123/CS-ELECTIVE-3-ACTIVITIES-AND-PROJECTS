e0f975ae75c7ae6cee8e58d25a047796693a7eab
Krisha De Silva <122845496+achlysha@users.noreply.github.com>
2026-01-16 19:14:11 +0800

    Update lab7.ino

--- lab7.ino
+++ lab7.ino
@@ -1,45 +1,61 @@
+// Pin Definitions
+const int redPin = 7;
+const int greenPin = 6;
+const int bluePin = 5;
 
-const int RED_LED = 7;
-const int GREEN_LED = 6;
-const int BLUE_LED = 5;
-
-bool redState = false;
-bool greenState = false;
-bool blueState = false;
+const int btn1 = 12; // Controls Red
+const int btn2 = 11; // Controls Green
+const int btn3 = 10; // Controls Blue
 
 void setup() {
-  pinMode(RED_LED, OUTPUT);
-  pinMode(GREEN_LED, OUTPUT);
-  pinMode(BLUE_LED, OUTPUT);
   Serial.begin(9600);
+  
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+
+  // Using PULLUP: button is LOW when pressed
+  pinMode(btn1, INPUT_PULLUP);
+  pinMode(btn2, INPUT_PULLUP);
+  pinMode(btn3, INPUT_PULLUP);
 }
 
 void loop() {
-  if (Serial.available()) {
-    char c = Serial.read();
-    c = tolower(c);
+  // 1. Handle Serial Commands (Inbound Signal)
+  if (Serial.available() > 0) {
+    char incomingByte = Serial.read();
+    
+    // Case insensitivity check (though using numbers, this handles stray chars)
+    incomingByte = tolower(incomingByte); 
 
-    if (c == '1') {
-      redState = !redState;
-      digitalWrite(RED_LED, redState);
+    if (incomingByte == '1') {
+      digitalWrite(redPin, !digitalRead(redPin));
+    } 
+    else if (incomingByte == '2') {
+      digitalWrite(greenPin, !digitalRead(greenPin));
+    } 
+    else if (incomingByte == '3') {
+      digitalWrite(bluePin, !digitalRead(bluePin));
     }
-    else if (c == '2') {
-      greenState = !greenState;
-      digitalWrite(GREEN_LED, greenState);
+    else if (incomingByte == 'o') { // Command for "All On"
+      digitalWrite(redPin, HIGH);
+      digitalWrite(greenPin, HIGH);
+      digitalWrite(bluePin, HIGH);
     }
-    else if (c == '3') {
-      blueState = !blueState;
-      digitalWrite(BLUE_LED, blueState);
-    }
-    else if (c == 'o') {
-      digitalWrite(RED_LED, HIGH);
-      digitalWrite(GREEN_LED, HIGH);
-      digitalWrite(BLUE_LED, HIGH);
-    }
-    else if (c == 'f') {
-      digitalWrite(RED_LED, LOW);
-      digitalWrite(GREEN_LED, LOW);
-      digitalWrite(BLUE_LED, LOW);
+    else if (incomingByte == 'f') { // Command for "All Off" (O for Off)
+      digitalWrite(redPin, LOW);
+      digitalWrite(greenPin, LOW);
+      digitalWrite(bluePin, LOW);
     }
   }
+
+  // 2. Handle Manual Button Presses (Extra logic for hardware completeness)
+  if (digitalRead(btn1) == LOW) { toggleLED(redPin); }
+  if (digitalRead(btn2) == LOW) { toggleLED(greenPin); }
+  if (digitalRead(btn3) == LOW) { toggleLED(bluePin); }
+}
+
+void toggleLED(int pin) {
+  digitalWrite(pin, !digitalRead(pin));
+  delay(250); // Debounce delay
 }
